1f1144c2064f04ea7515dcfbd733174ca1613341
skrool <206881066+skroolll@users.noreply.github.com.>
2026-03-12 20:47:59 +0700

    функция чтения из файла

--- labs/lab11_io/Lab11/src/main.cpp
+++ labs/lab11_io/Lab11/src/main.cpp
@@ -11,7 +11,7 @@ const int BUFFER_SIZE = 1024;
 
 struct WordInfo{
     char word[MAX_LEN+1];
-    int lenght;
+    int length;
     int rootFor[MAX_WORDS];
     int rootForCount;
     bool isRoot;
@@ -31,12 +31,39 @@ bool isRootWord(const char* root, const char* word) {
 //чтение из файла
 int readWordsFromFile(const char* filer, WordInfo words[]) {
     FILE* file = fopen(filer, "r");
-    
+
     if (filer == NULL) {
         cout << "Ошибка открытия файла " << filer << endl;
         return 0;
     }
 
+
+    char buffer[BUFFER_SIZE];
+    int wordCount = 0;
+
+    while (fgets(buffer, BUFFER_SIZE, file) != NULL && wordCount < MAX_WORDS) {
+
+        for (int i = 0; buffer[i] != '\0' && wordCount < MAX_WORDS; i++) {
+            if (isspace(buffer[i]))
+                continue;
+
+            int j = 0;
+            while (!isspace(buffer[i]) && buffer[i] != '\0' && j < MAX_LEN) {
+                words[wordCount].word[j] = buffer[i];
+                i++;
+                j++;
+            }
+
+            words[wordCount].word[j] = '\0';
+            words[wordCount].length = j;
+            words[wordCount].rootForCount = 0;
+            words[wordCount].isRoot = false;
+            wordCount++;
+
+        }
+    }
+    fclose(file);
+    return wordCount;
 }
 
 int main() {
