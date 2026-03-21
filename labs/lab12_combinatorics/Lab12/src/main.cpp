7273afed59e9d9eebad9690114aa1d208d608142
skrool <206881066+skroolll@users.noreply.github.com.>
2026-03-22 02:14:05 +0700

    решение матрицой

--- labs/lab12_combinatorics/Lab12/src/main.cpp
+++ labs/lab12_combinatorics/Lab12/src/main.cpp
@@ -29,6 +29,43 @@ void printBoard(const vector<int> & boardpro, int k) {
 }
 
 
+
+//матрица в лоб
+bool proverka_v_lob(const vector <vector<int>> & b, int st, int stl, int raz) {
+    for (int i = 1; i <= st; ++i) {
+        if (b[st - i][stl]==1) return false; // вверх
+        if (stl - i >= 0 && b[st - i][stl - i]==1) return false; // диагональ влево
+        if (stl + i < raz && b[st - i][stl + i]==1) return false;  // диагональ вправо
+    }
+    return true;
+}
+
+void v_lob(vector <vector<int>> & board, int st, int raz, int& count) {
+    if (st == raz) {
+        count++;
+        if (raz<=5) printBoard(board, raz);
+        return;
+    }
+
+    for (int stl = 0; stl < raz; ++stl) {
+        if (proverka_v_lob(board, st, stl, raz)) {
+            board[st][stl] = 1;
+            v_lob(board, st + 1, raz, count);
+            board[st][stl] = 0;  // backtracking
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 int main() {
 
     return 0;
