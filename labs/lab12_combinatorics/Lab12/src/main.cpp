e81ed3e8a230ba92c9d3d68c3a6bad1c24bfc575
skrool <206881066+skroolll@users.noreply.github.com.>
2026-03-22 01:24:31 +0700

    сделал выводы для обоих способов

--- labs/lab12_combinatorics/Lab12/src/main.cpp
+++ labs/lab12_combinatorics/Lab12/src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <vector> 
+using namespace std;
 
-// Lab 12
-// TODO: реализуйте решение по заданию в labs/lab12_combinatorics/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+
+//выводы 
+void printBoard(const vector <vector<int>> & board, int k) {
+    for (int i = 0; i < k; ++i, cout << endl)
+        for (int j = 0; j < k; ++j) 
+            if (board[i][j]==1){
+                cout << "Q ";
+            }
+            else{
+                cout << ". ";
+            }
+    cout << endl;
+}
+
+void printBoard(const vector<int> & boardpro, int k) {
+    for (int i = 0; i < k; ++i, cout << endl)
+        for (int j = 0; j < k; ++j) 
+            if (boardpro[j]==i){
+                cout << "Q ";
+            }
+            else{
+                cout << ". ";
+            }
+    cout << endl;
+}
+
+
 int main() {
-    // TODO
+
     return 0;
 }
