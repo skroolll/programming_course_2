2330017b55044d09a925797b2b5c910a93c6c573
skrool <206881066+skroolll@users.noreply.github.com.>
2026-04-16 20:51:35 +0700

    1 упражнение обяз лабы

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-// Lab 14
-// TODO: реализуйте решение по заданию в labs/lab14_graphs/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+
 int main() {
-    // TODO
+
+    ifstream in1("input.txt");
+    int u, v, max = 0;
+    vector<pair<int, int>> edgeList;
+
+    while (in1 >> u >> v) {
+        edgeList.push_back({u, v});
+        if (u > max) max = u;
+        if (v > max) max = v;
+    }
+    in1.close();
+
+    //создание и заполнение матр.смежн.
+    vector<vector<int>> matrix(max, vector<int>(max, 0));
+    for (auto edge : edgeList) {
+        matrix[edge.first - 1][edge.second - 1] = 1;
+        matrix[edge.second - 1][edge.first - 1] = 1; 
+    }
+
+    // запись в output1.txt
+    ofstream out1("output1.txt");
+    out1 << max << endl; 
+    for (int i = 0; i < max; i++) {
+        for (int j = 0; j < max; j++) {
+            out1 << matrix[i][j] << " ";
+        }
+        out1 << endl;
+    }
+    out1.close();
+
     return 0;
 }
